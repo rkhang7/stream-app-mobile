7c2f08888d0b9f62b0f5eb57d38872e9ad6edca3
rkhang7 <khang220300@gmail.com>
2021-10-12 23:58:39 +0700

    update UI

--- app/src/main/java/com/iuh/stream/activity/FriendProfileActivity.java
+++ app/src/main/java/com/iuh/stream/activity/FriendProfileActivity.java
@@ -5,11 +5,15 @@ import androidx.constraintlayout.widget.Guideline;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.iuh.stream.R;
 import com.iuh.stream.models.User;
+
+import java.text.SimpleDateFormat;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -18,7 +22,8 @@ public class FriendProfileActivity extends AppCompatActivity {
 
     // views
     private CircleImageView avtIv;
-    private TextView nameTv;
+    private TextView nameTv, dobTv, genderTv, phoneNumberTv, emailTv;
+    private LinearLayout emailLayout, phoneNumberLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +34,7 @@ public class FriendProfileActivity extends AppCompatActivity {
     private void addControls() {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         user = (User) getIntent().getSerializableExtra(AddFriendActivity.USER_KEY);
+        Log.e("TAG" , "addControls: "+ user);
 
         // init views
         avtIv = findViewById(R.id.avt_iv);
@@ -36,6 +42,35 @@ public class FriendProfileActivity extends AppCompatActivity {
 
         nameTv = findViewById(R.id.name_tv);
         nameTv.setText(user.getFirstName() + " " +  user.getLastName());
+
+        dobTv = findViewById(R.id.dob_tv);
+        // datetime format
+        String pattern = "dd-MM-yyyy";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+        dobTv.setText(simpleDateFormat.format(user.getDateOfBirth()));
+
+        genderTv = findViewById(R.id.gender_tv);
+        genderTv.setText(user.getGender());
+
+        phoneNumberTv = findViewById(R.id.phone_number_tv);
+        emailTv = findViewById(R.id.email_tv);
+        phoneNumberLayout = findViewById(R.id.phone_layout);
+        emailLayout = findViewById(R.id.email_layout);
+        if(user.getPhoneNumber() != null){
+            emailLayout.setVisibility(View.GONE);
+            phoneNumberLayout.setVisibility(View.VISIBLE);
+            phoneNumberTv.setText(user.getPhoneNumber());
+        }
+
+        else if(!user.getEmail().equals("null")){
+            emailLayout.setVisibility(View.VISIBLE);
+            phoneNumberLayout.setVisibility(View.GONE);
+            emailTv.setText(user.getEmail());
+        }
+
+
+
+
 
     }
 
