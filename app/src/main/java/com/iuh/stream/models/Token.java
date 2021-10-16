d07b8c59c86d94bd22f8a0d7acba18738a17db71
rkhang7 <khang220300@gmail.com>
2021-10-16 09:46:37 +0700

    get token

---
+++ app/src/main/java/com/iuh/stream/models/Token.java
@@ -0,0 +1,38 @@
+package com.iuh.stream.models;
+
+public class Token {
+    private String accessToken;
+    private String refreshToken;
+
+    public Token() {
+    }
+
+    public Token(String accessToken, String refreshToken) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    @Override
+    public String toString() {
+        return "Token{" +
+                "accessToken='" + accessToken + '\'' +
+                ", refreshToken='" + refreshToken + '\'' +
+                '}';
+    }
+}
