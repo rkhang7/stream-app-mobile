a80603c160ca0e25e3958633576b3830e59d7ef4
rkhang7 <khang220300@gmail.com>
2021-11-04 20:57:44 +0700

    delete friend, add swiperefreshlayout

--- app/src/main/java/com/iuh/stream/adapter/FriendsAdapter.java
+++ app/src/main/java/com/iuh/stream/adapter/FriendsAdapter.java
@@ -1,28 +1,44 @@
 package com.iuh.stream.adapter;
 
+import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
 import com.iuh.stream.R;
+import com.iuh.stream.api.RetrofitService;
+import com.iuh.stream.datalocal.DataLocalManager;
+import com.iuh.stream.dialog.CustomAlert;
 import com.iuh.stream.models.User;
+import com.iuh.stream.utils.Constants;
+import com.iuh.stream.utils.Util;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class FriendsAdapter extends RecyclerView.Adapter<FriendsAdapter.FiendsViewHolder>{
     private Context mContext;
     private List<User> userList;
+    private FirebaseAuth mAuth;
 
     public FriendsAdapter(Context mContext) {
+        mAuth = FirebaseAuth.getInstance();
         this.mContext = mContext;
     }
 
@@ -57,6 +73,100 @@ public class FriendsAdapter extends RecyclerView.Adapter<FriendsAdapter.FiendsVi
             super(itemView);
             avatarIv = itemView.findViewById(R.id.friend_avatar_iv);
             nameTv = itemView.findViewById(R.id.friend_name_tv);
+
+            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    openDialog(getAdapterPosition());
+                    return false;
+                }
+            });
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText((Activity)mContext, "Click", Toast.LENGTH_SHORT).show();
+
+                }
+            });
         }
     }
+
+    private void openDialog(int position) {
+        User user = userList.get(position);
+        String senderId = mAuth.getCurrentUser().getUid();
+        String receiverId = user.get_id();
+        String accessToken = DataLocalManager.getStringValue(Constants.ACCESS_TOKEN);
+        final String OPTION = "friend";
+
+        // set up dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+
+        String name = user.getFirstName() + " " + user.getLastName();
+        builder.setTitle(name);
+        String[] options = {"Xem thông tin", "Chăn người này", "Xóa bạn"};
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which){
+                    case 0:
+                        CustomAlert.showToast((Activity) mContext, CustomAlert.INFO, "Xem thông tin");
+                        break;
+                    case 1:
+                        CustomAlert.showToast((Activity) mContext, CustomAlert.INFO, "Tính năng này chưa đươc phát triển");
+                        break;
+                    case 2:
+                        openConfirmDialog(name, senderId, receiverId, OPTION, accessToken, position);
+                        break;
+
+                }
+            }
+        });
+        builder.create().show();
+    }
+
+    private void openConfirmDialog(String name, String senderId, String receiverId, String option, String accessToken, int position) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+        builder.setTitle("Xóa bạn với " + name);
+        builder.setNegativeButton("Hủy", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.setPositiveButton("Xóa", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                deleteFriend(senderId, receiverId, option, accessToken, position);
+            }
+        });
+
+        builder.create().show();
+    }
+
+
+    private void deleteFriend(String senderId, String receiverId, String option, String accessToken, int position) {
+        RetrofitService.getInstance.deleteUserIDByOption(senderId, receiverId, option, accessToken)
+                .enqueue(new Callback<Void>() {
+                    @Override
+                    public void onResponse(Call<Void> call, Response<Void> response) {
+                        if (response.isSuccessful()) {
+                            userList.remove(position);
+                            notifyItemRemoved(position);
+                        } else if (response.code() == 500) {
+                            CustomAlert.showToast((Activity) mContext, CustomAlert.WARNING, "Đã xảy ra lỗi");
+                        } else if (response.code() == 403) {
+                            Util.refreshToken(DataLocalManager.getStringValue(Constants.REFRESH_TOKEN));
+                            deleteFriend(senderId, receiverId, option, accessToken, position);
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<Void> call, Throwable t) {
+                        CustomAlert.showToast((Activity) mContext, CustomAlert.WARNING, t.getMessage());
+                    }
+                });
+    }
+
+
 }
--- app/src/main/java/com/iuh/stream/fragment/PersonalContactFragment.java
+++ app/src/main/java/com/iuh/stream/fragment/PersonalContactFragment.java
@@ -1,13 +1,17 @@
 package com.iuh.stream.fragment;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -38,19 +42,21 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 
 
-public class PersonalContactFragment extends Fragment {
+public class PersonalContactFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener{
     // views
     private View view;
     private Button friendFromContactBtn;
     private Button friendInvitationBtn;
-    private List<String> listFriendId;
-    private List<User> listFriendUser;
+    private List<String> listFriendId ,listFriendIdUpdate;
+    private List<User> listFriendUser, listFriendUserUpdate;
     private FriendsAdapter friendsAdapter;
     private RecyclerView recyclerView;
-    private TextView updateFriendsTv;
     private User user;
+    private static final int UPDATE = 1;
+    private static final int LOAD = 2;
 
     private FirebaseAuth mAuth;
+    private SwipeRefreshLayout swipeRefreshLayout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -98,20 +104,14 @@ public class PersonalContactFragment extends Fragment {
                 startActivity(intent);
             }
         });
-
-        updateFriendsTv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
     }
 
     private void addControls() {
         // init views
         friendFromContactBtn = view.findViewById(R.id.friend_from_contact_btn);
         friendInvitationBtn = view.findViewById(R.id.friend_invitation_btn);
-        updateFriendsTv = view.findViewById(R.id.update_friend_tv);
+        swipeRefreshLayout = view.findViewById(R.id.swipeRefreshLayout);
+        swipeRefreshLayout.setColorSchemeColors(getResources().getColor(R.color.main));
 
         mAuth = FirebaseAuth.getInstance();
         listFriendId = new ArrayList<>();
@@ -120,51 +120,70 @@ public class PersonalContactFragment extends Fragment {
         friendsAdapter.setData(listFriendUser);
         recyclerView = view.findViewById(R.id.personal_contacts_rcv);
         recyclerView.setAdapter(friendsAdapter);
+        RecyclerView.ItemDecoration itemDecoration = new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.addItemDecoration(itemDecoration);
+        swipeRefreshLayout.setOnRefreshListener(this);
 
-        loadContacts();
+
+        loadContacts(LOAD);
     }
 
-    private void loadContacts() {
+    private void loadContacts(int type) {
+        listFriendId.clear();
+        listFriendUser.clear();
         RetrofitService.getInstance.getMeInfo(DataLocalManager.getStringValue(Constants.ACCESS_TOKEN))
                 .enqueue(new Callback<User>() {
                     @Override
                     public void onResponse(Call<User> call, Response<User> response) {
                         if (response.code() == 403) {
                             Util.refreshToken(DataLocalManager.getStringValue(Constants.REFRESH_TOKEN));
-                            loadContacts();
+                            loadContacts(LOAD);
                         } else {
                             user = response.body();
                             listFriendId = user.getContacts();
-                            for (String id : listFriendId) {
-                                getListFriendUser(id);
+                            if(listFriendId.size() > 0){
+                                for (String id : listFriendId) {
+                                    getListFriendUser(id, type);
+                                }
                             }
+                            else{
+                                friendsAdapter.setData(listFriendUser);
+                                recyclerView.setAdapter(friendsAdapter);
+                                Log.e("TAG", "updated: "  );
+                                if(type == UPDATE){
+                                    swipeRefreshLayout.setRefreshing(false);
+                                }
+                            }
+
                         }
                     }
 
                     @Override
                     public void onFailure(Call<User> call, Throwable t) {
-
+                        CustomAlert.showToast(getActivity(), CustomAlert.WARNING, t.getMessage());
                     }
                 });
     }
 
-    private void getListFriendUser(String id) {
+    private void getListFriendUser(String id, int type) {
         RetrofitService.getInstance.getUserById(id, DataLocalManager.getStringValue(Constants.ACCESS_TOKEN))
                 .enqueue(new Callback<User>() {
                     @Override
                     public void onResponse(Call<User> call, Response<User> response) {
                         if (response.code() == 403) {
                             Util.refreshToken(DataLocalManager.getStringValue(Constants.REFRESH_TOKEN));
-                            getListFriendUser(id);
+                            getListFriendUser(id, type);
                         } else {
                             user = response.body();
                             listFriendUser.add(user);
                         }
                         friendsAdapter.setData(listFriendUser);
-                        Log.e("TAG", "onResponse: " + listFriendUser );
                         recyclerView.setAdapter(friendsAdapter);
-
+                        Log.e("TAG", "updated: "  );
+                        if(type == UPDATE){
+                            swipeRefreshLayout.setRefreshing(false);
+                        }
                     }
 
                     @Override
@@ -175,4 +194,8 @@ public class PersonalContactFragment extends Fragment {
     }
 
 
+    @Override
+    public void onRefresh() {
+        loadContacts(UPDATE);
+    }
 }
